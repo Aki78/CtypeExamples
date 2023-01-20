c02cf4611094255c788abc2790135170ac3f8733
Aki78 <kmorooka78@gmail.com>
2023-01-20 14:18:19 +0300

    corrected function name

--- example.py
+++ example.py
@@ -4,7 +4,7 @@ import ctypes
 my_lib = ctypes.CDLL('./path/to/library.so')
 
 # Declare the function prototype
-my_lib.add_numbers.argtypes = [ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double)]
+my_lib.plus.argtypes = [ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double)]
 
 # Input numbers as pointers
 num1 = ctypes.c_double(1.5)
@@ -12,7 +12,7 @@ num2 = ctypes.c_double(2.5)
 result = ctypes.c_double()
 
 # Call the function
-my_lib.add_numbers(ctypes.byref(num1), ctypes.byref(num2), ctypes.byref(result))
+my_lib.plus(ctypes.byref(num1), ctypes.byref(num2), ctypes.byref(result))
 
 # Print the result
 print(result.value)
